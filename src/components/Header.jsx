b606322e6fd722c61c589c35a2002acaec529642
jsanders108 <74698177+jsanders108@users.noreply.github.com>
2022-11-19 11:24:17 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header(){
     return (
         <div className="header--container">
-            <img className="" src="" />
+            <img className="header--image" src="./src/" />
             <h1 className="header--title">Country Info App</h1>
         </div>
     )
